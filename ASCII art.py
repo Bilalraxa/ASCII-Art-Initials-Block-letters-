40a757ded9db4e8e9abb6b5a29d6d4dadb91462f
Bilalraxa <105665747+Bilalraxa@users.noreply.github.com>
2024-07-16 17:37:42 +0500

    Add files via upload

---
+++ ASCII art.py
@@ -0,0 +1,24 @@
+# initials.py
+# Bilal Raza
+#I am a History-inthusiast
+
+# Define a function named 'print_initials'
+def print_initials():
+    # Print each line of the initials "B" and "R" in block letters using ASCII art
+    print("BBBB   RRRR ")
+    print("B   B  R   R")
+    print("B   B  R   R")
+    print("BBBB   RRRR ")
+    print("B   B  R R  ")
+    print("B   B  R  R ")
+    print("BBBB   R   R")
+
+# This code block will only run if this file is executed directly
+if __name__ == "__main__":
+    # Call the function 'print_initials' to display the initials
+    print_initials()
+
+
+
+
+
